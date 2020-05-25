6df76c24c4940d4b166251c7a77f5af6a7850728
erak1609 <eva.rakova@mail.schwarz>
2020-05-25 23:57:17 +0300

    NgRx communication between product-edit and list components; Load action

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
-import { Subscription } from 'rxjs';
-
 import { Product } from '../product';
 import { ProductService } from '../product.service';
 import { Store, select } from '@ngrx/store';
@@ -23,16 +21,16 @@ export class ProductListComponent implements OnInit, OnDestroy {
 
   // Used to highlight the selected product in the list
   selectedProduct: Product | null;
-  sub: Subscription;
+ 
 
   constructor(private productService: ProductService, 
     private store: Store<fromProduct.Sate>){ }
 
   ngOnInit(): void {
-    this.sub = this.productService.selectedProductChanges$.subscribe(
-      selectedProduct => this.selectedProduct = selectedProduct
-    );
-
+   this.store.pipe(
+     select(fromProduct.getCurrentProduct))
+     .subscribe(cp => this.selectedProduct = cp);
+  
     this.productService.getProducts().subscribe({
       next: (products: Product[]) => this.products = products,
       error: (err: any) => this.errorMessage = err.error
@@ -47,20 +45,19 @@ export class ProductListComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.sub.unsubscribe();
+   
   }
 
   checkChanged(value: boolean): void {
     this.store.dispatch(new productActions.ToggleProductCode(value));
 }
   
-
   newProduct(): void {
-    this.productService.changeSelectedProduct(this.productService.newProduct());
+   this.store.dispatch(new productActions.InitializeCurrentProduct());
   }
 
   productSelected(product: Product): void {
-    this.productService.changeSelectedProduct(product);
+  this.store.dispatch(new productActions.SetCurrentProduct(product));
   }
 
 }
--- src/app/products/state/product.action.ts
+++ src/app/products/state/product.action.ts
@@ -5,7 +5,10 @@ export enum ProductActionsTypes{
     ToggleProductCode = '[Products] Toggle Product Code',
     SetCurrentProduct = '[Products] Set Current Product',
     ClearCurrentProduct = '[Products] Clear Current Product',
-    InitializeCurrentProduct = '[Products] Initialize Current Product'
+    InitializeCurrentProduct = '[Products] Initialize Current Product',
+    Load = '[Products] Load',
+    LoadSuccess = '[Products] Load Success',
+    LoadFail = '[Products] Load Fail'
 }
 
 export class ToggleProductCode implements Action{
@@ -28,4 +31,20 @@ export class InitializeCurrentProduct implements Action{
     readonly type = ProductActionsTypes.InitializeCurrentProduct;
 }
 
-export type ProductActions = ToggleProductCode | ClearCurrentProduct | SetCurrentProduct | InitializeCurrentProduct;
+export class Load implements Action{
+    readonly type = ProductActionsTypes.Load;
+}
+
+export class LoadSuccess implements Action{
+    readonly type = ProductActionsTypes.LoadSuccess;
+
+    constructor(public payload: Product[]){}
+}
+
+export class LoadFail implements Action{
+    readonly type = ProductActionsTypes.LoadFail;
+
+    constructor(public payload: string){}
+}
+
+export type ProductActions = ToggleProductCode | ClearCurrentProduct | SetCurrentProduct | InitializeCurrentProduct | Load | LoadSuccess | LoadFail;
